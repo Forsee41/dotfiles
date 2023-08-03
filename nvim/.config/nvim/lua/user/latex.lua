38a7830454b987ed150436f80b4f5d1a10b1d089
Forsee41 <forsee41@gmail.com>
2023-08-03 11:45:35 +0300

    changes

---
+++ nvim/.config/nvim/lua/user/latex.lua
@@ -0,0 +1,5 @@
+vim.cmd("filetype plugin indent on")
+vim.cmd("let g:vimtex_view_general_viewer = 'okular'")
+vim.cmd("let g:vimtex_view_method = 'zathura'")
+-- vim.cmd("let g:vimtex_compiler_method = 'latexrun'")
+vim.cmd("let maplocalleader = '\\'")
---
+++ nvim/.config/nvim/lua/user/md-preview.lua
@@ -0,0 +1,2 @@
+vim.cmd "let g:mkdp_auto_start = 1"
+vim.cmd "let g:mkdp_auto_close = 1"
